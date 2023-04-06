ff21a21bbc5da910328ee723a466db59beb21a60
baggy102 <rlgh0806@gmail.com>
2023-04-06 17:43:38 +0900

    mvc_mysqql post

--- 13_mvc_mysql/routes/index.js
+++ 13_mvc_mysql/routes/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 router.get('/', controller.main);
 router.get('/visitor', controller.getVisitors);
 
+// POST / visitor/ write
+router.post('/visitor/write', controller.postVisitor); // 요청이 들어오면 controller에 postVisitor 실행 시키겠다, 하나 추가
+
 module.exports = router;
