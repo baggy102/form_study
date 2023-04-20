c6e888795dd4e7250f080ac03c0f4f2c651ef0cf
baggy102 <rlgh0806@gmail.com>
2023-04-20 17:29:39 +0900

    react 합성이벤트

---
+++ 22_react_04_eventhandling/src/SyntheticEvent.js
@@ -0,0 +1,21 @@
+function SyntheticEvent() {
+  function syntheticEvent(e) {
+    // Synthetic Event (합성 이벤트)
+    // 웹 브라우저의 naticeEvent가 아니라 natice Event를 사용함
+    // 즉, 리액트 고유 이벤트 객체!
+    console.log(e);
+  }
+
+  function printInputValue(e) {
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <button onClick={syntheticEvent}>합성이벤트 콘솔 찍기</button>
+      <input type="text" placeholder="아무거나 입력!" onChange={printInputValue} />
+    </div>
+  );
+}
+
+export default SyntheticEvent;
