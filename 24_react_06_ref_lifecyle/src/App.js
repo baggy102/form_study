3de60c254ebf65e5c74f562fa67cb541be7132d5
baggy102 <rlgh0806@gmail.com>
2023-04-22 15:06:52 +0900

    study:ref

---
+++ 24_react_06_ref_lifecyle/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RefSample3 from './RefSample3';
+import RefSample4 from './RefSample4';
+
+function App() {
+  return (
+    <div className="App">
+      <RefSample4></RefSample4>
+    </div>
+  );
+}
+
+export default App;
