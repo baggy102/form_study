df7eeeeb17fb4cbefe0c070fbdd5de09267419de
baggy102 <rlgh0806@gmail.com>
2023-04-26 18:49:14 +0900

    study:sass

---
+++ 26_reacct_06_sass/src/Animation.js
@@ -0,0 +1,13 @@
+import './styles/Animation.scss';
+
+function Animation() {
+  return (
+    <div className="circle-container">
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+    </div>
+  );
+}
+
+export default Animation;
---
+++ 26_reacct_06_sass/src/App.js
@@ -0,0 +1,15 @@
+import Larva from './Larva';
+import Animation from './Animation';
+import Image from './Image';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Larva /> */}
+      {/* <Animation /> */}
+      <Image />
+    </div>
+  );
+}
+
+export default App;
